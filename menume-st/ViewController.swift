85ca142f61f3638f820d77810ea77bea1caf3727
emmalla <emmalla@bu.edu>
2022-06-13 11:55:27 -0400

    init calendar functionality, currently can only assign every day the same menu item by clicking edit button

--- menume-st/ViewController.swift
+++ menume-st/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import FSCalendar
+import Foundation
+
+let dateFormatter = DateFormatter()
 
 class ViewController: UIViewController {
+    
+    var currDate:Date? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var menuList:[String:[String]] = [:]
+
+    var dayMenuList:[Date:String] = [:]
+    
+    var dayEventsList:[Date:[String]] = [:]
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    
+    @IBOutlet weak var eventsLabel: UILabel!
+    
+    @IBOutlet weak var menuLabel: UILabel!
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        
+        dayMenuList[currDate!] = "tofu masala"
+        menuLabel.text = dayMenuList[currDate!] 
     }
-
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        editButton.titleLabel?.text = "Add"
+        
+        // Do any additional setup after loading the view.
+        
+        calendar.scrollDirection = .vertical
+        calendar.scope = .month
+        
+    }
 
 }
 
+extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("date selected", date)
+        currDate = date
+        dateFormatter.string(from: currDate!)
+        print(dayMenuList)
+        if (dayMenuList[currDate!] != nil){
+            menuLabel.text = dayMenuList[currDate!]
+        }else {
+            menuLabel.text = "no menu selected, add below"
+        }
+    
+    }
+}
